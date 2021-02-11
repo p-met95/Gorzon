a590ad6f6363d5b4582e177bd77783440ac263d9
pmet <robertofrench673@gmail.com>
2021-02-11 18:11:07 -0500

    added heroku reqs

--- bot/main.py
+++ bot/main.py
@@ -2,20 +2,20 @@ import discord
 from discord.ext import commands
 import os
 
-client = commands.Bot(command_prefix=".")
+client = commands.Bot(command_prefix="!")
 token = os.getenv("DISCORD_BOT_TOKEN")
 
 @client.event
-async def on_ready() :
-    await client.change_presence(status = discord.Status.idle, activity = discord.Game("Listening to .help"))
+async def on_ready():
+    await client.change_presence(status = discord.Status.idle, activity = discord.Game("READY TO DEVOUR PUNY HUMANS"))
     print("I am online")
 
 @client.command()
-async def ping(ctx) :
+async def ping(ctx):
     await ctx.send(f"🏓 Pong with {str(round(client.latency, 2))}")
 
 @client.command(name="whoami")
-async def whoami(ctx) :
+async def whoami(ctx):
     await ctx.send(f"You are {ctx.message.author.name}")
 
 @client.command()
@@ -23,7 +23,7 @@ async def clear(ctx, amount=3) :
     await ctx.channel.purge(limit=amount)
 
 @client.command(name="specific")
-async def whoami(ctx) :
+async def whoami(ctx):
     await ctx.send(f"You are {ctx.message.author.name}")
 
 
